38633b2d930c572d9dbc7f1fe1947c957d24c4a1
April Tran <arpil@HW-Mac.fios-router.home>
2020-08-08 10:40:18 -0400

    add db & auth

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,31 @@
 from flask_wtf import FlaskForm
 from wtforms import BooleanField, PasswordField, StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
+
+from app.models import User
 
 class LoginForm(FlaskForm):
     username = StringField('username', validators=[DataRequired()])
-    password =  PasswordField('password', validators=[DataRequired()])
+    password = PasswordField('password', validators=[DataRequired()])
     remember_me = BooleanField('remember me')
     submit = SubmitField('log in')
+
+class RegistrationForm(FlaskForm):
+    username = StringField('username', validators=[DataRequired()])
+    email = StringField('email', validators=[DataRequired(), Email()])
+    password = PasswordField('password', validators=[DataRequired()])
+    password_confirm = PasswordField(
+        'confirm password',
+        validators=[DataRequired(), EqualTo('password')]
+    )
+    submit = SubmitField('register')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('ur gonna need a more unique username.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('this email is already registered.')
